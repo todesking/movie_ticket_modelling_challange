9df8e27f3b03c6cad159237ee6cc62835ce865fa
todesking <discommunicative@gmail.com>
2019-07-16 00:52:55 +0900

    refactor app

---
+++ src/main/scala/application/AppUtil.scala
@@ -0,0 +1,40 @@
+package mtmc.application
+
+import mtmc.domain.Show
+import mtmc.domain.DateTime
+
+object AppUtil {
+  def readInt() = {
+    print("> ")
+    scala.io.StdIn.readLine().toInt
+  }
+
+  // 本来なら上映検索サービスからから本日分の未上映回を取得することになりそうだが、本質ではないので省略
+  val availableShows = Seq(
+    Show(DateTime(false, false, false), false),
+    Show(DateTime(false, false, false), true),
+    Show(DateTime(false, false, true), false),
+    Show(DateTime(false, false, true), true),
+    Show(DateTime(false, true, false), false),
+    Show(DateTime(false, true, false), true),
+    Show(DateTime(false, true, true), false),
+    Show(DateTime(false, true, true), true),
+    Show(DateTime(true, false, false), false),
+    Show(DateTime(true, false, false), true),
+    Show(DateTime(true, false, true), false),
+    Show(DateTime(true, false, true), true),
+    Show(DateTime(true, true, false), false),
+    Show(DateTime(true, true, false), true),
+    Show(DateTime(true, true, true), false),
+    Show(DateTime(true, true, true), true))
+
+  // これはアプリケーションの責務でええやろ
+  def pretty(s: Show): String = {
+    val date = if (s.isWeekday) "平日" else "土日祝"
+    val dateEx = if (s.isCinemaDay) "かつ映画の日" else ""
+    val time = if (s.startAt.isLate) "20時以降" else "20時以前"
+    val loud = if (s.isLoudSoundShow) ", 爆音上映" else ""
+    s"$date$dateEx, $time$loud"
+  }
+
+}
--- src/main/scala/application/TicketVendor.scala
+++ src/main/scala/application/TicketVendor.scala
@@ -1,42 +1,11 @@
 package mtmc.application
 
-import mtmc.domain.Show
-import mtmc.domain.DateTime
 import mtmc.domain.Ticket
 
 object TicketVendor {
-  private[this] def readInt() = {
-    print("> ")
-    scala.io.StdIn.readLine().toInt
-  }
+  import AppUtil.{readInt, pretty}
 
-  // 本来なら上映検索サービスからから本日分の未上映回を取得することになりそうだが、本質ではないので省略
-  val shows = Seq(
-    Show(DateTime(false, false, false), false),
-    Show(DateTime(false, false, false), true),
-    Show(DateTime(false, false, true), false),
-    Show(DateTime(false, false, true), true),
-    Show(DateTime(false, true, false), false),
-    Show(DateTime(false, true, false), true),
-    Show(DateTime(false, true, true), false),
-    Show(DateTime(false, true, true), true),
-    Show(DateTime(true, false, false), false),
-    Show(DateTime(true, false, false), true),
-    Show(DateTime(true, false, true), false),
-    Show(DateTime(true, false, true), true),
-    Show(DateTime(true, true, false), false),
-    Show(DateTime(true, true, false), true),
-    Show(DateTime(true, true, true), false),
-    Show(DateTime(true, true, true), true))
-
-  // これはアプリケーションの責務でええやろ
-  def pretty(s: Show): String = {
-    val date = if (s.isWeekday) "平日" else "土日祝"
-    val dateEx = if (s.isCinemaDay) "かつ映画の日" else ""
-    val time = if (s.startAt.isLate) "20時以降" else "20時以前"
-    val loud = if (s.isLoudSoundShow) ", 爆音上映" else ""
-    s"$date$dateEx, $time$loud"
-  }
+  val shows = AppUtil.availableShows
 
   def main(args: Array[String]): Unit = {
     println("これはチケット販売機です")
